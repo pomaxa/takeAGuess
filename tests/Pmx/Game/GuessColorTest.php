762b5615a3ade4314fc0a5bfa66e27c30d200b47
pomaxa <pomaxa@gmail.com>
2015-05-31 20:35:04 +0300

    update test to check score

--- tests/Pmx/Game/GuessColorTest.php
+++ tests/Pmx/Game/GuessColorTest.php
@@ -14,8 +14,10 @@ class GuessColorTest extends \PHPUnit_Framework_TestCase
         $c = $deck->pick();
         $color = in_array(substr($c, -1), array('k', 'p')) ? 'black' : 'red';
 
+        self::assertTrue($game->getScore() === 0);
+
         $result = $game->takeAGuess($color);
         self::assertTrue($result);
+        self::assertTrue($game->getScore() === 1);
     }
-
 }
